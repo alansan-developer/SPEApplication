7608a25d4cbedff49ad67223a1e43bc37f75e3e0
Alan <alansan96@hotmail.com>
2020-08-06 13:51:52 +0700

    Initialize

---
+++ SPEApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPEApplication
+//
+//  Created by Alan Santoso on 06/08/20.
+//  Copyright © 2020 Alan Santoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
